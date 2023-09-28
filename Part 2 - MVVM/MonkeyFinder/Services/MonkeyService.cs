f24e881ec0ac43eb057bc6d1ac789c39619926a1
Chamakura Raghu Nandan Reddy <raghu2000pers@gmail.com>
2023-09-27 22:07:18 -0500

    Unsealed Part-2
    Updated the given to execute the required output.

---
+++ Part 2 - MVVM/MonkeyFinder/Services/MonkeyService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
+
+public class MonkeyService
+{
+    HttpClient httpclient;
+    public MonkeyService()
+    {
+        this.httpclient=new HttpClient ();
+    }
+    List<Monkey> monkeyList = new List<Monkey>();
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if(monkeyList?.Count > 0)
+        {
+            return monkeyList;
+        }
+        var response = await httpclient.GetAsync("https://www.montemagno.com/monkeys.json");
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync(MonkeyContext.Default.ListMonkey);
+        }
+
+        using var stream = await FileSystem.OpenAppPackageFileAsync("monkeydata.json");
+        using var reader=new StreamReader(stream);
+        var contents=await reader.ReadToEndAsync();
+        monkeyList = JsonSerializer.Deserialize(contents, MonkeyContext.Default.ListMonkey);
+        return monkeyList;
+    }
+}
